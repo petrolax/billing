bd6ab95104b8b44ef2906154f8e3dfad0e6a0741
petrolax <poa21y@gmail.com>
2021-08-11 13:25:36 +0300

    Changed and added some methods of handler

--- gateway/handler/handler.go
+++ gateway/handler/handler.go
@@ -3,24 +3,73 @@ package handler
 import (
 	"encoding/json"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	tr "github.com/petrolax/billing/transaction"
 	"github.com/streadway/amqp"
+	"github.com/teris-io/shortid"
 )
 
+type Logger interface {
+	Print(v ...interface{})
+	Println(v ...interface{})
+	Printf(format string, v ...interface{})
+	Fatal(v ...interface{})
+	Fatalln(v ...interface{})
+	Fatalf(format string, v ...interface{})
+	Panic(v ...interface{})
+	Panicln(v ...interface{})
+	Panicf(format string, v ...interface{})
+	Output(calldepth int, s string) error
+}
+
 type Handler struct {
-	ch  *amqp.Channel
-	log *log.Logger
+	ch    *amqp.Channel
+	log   *log.Logger
+	queue [3]amqp.Queue
+	msgs  [3]<-chan amqp.Delivery
 }
 
 func NewHandler(ch *amqp.Channel) *Handler {
+	var queue [3]amqp.Queue
+	var err error
+	for i := 0; i < len(queue); i++ {
+		queue[i], err = ch.QueueDeclare(
+			"",
+			false,
+			false,
+			true,
+			false,
+			nil,
+		)
+		if err != nil {
+			log.Fatalln("Withdraw:Error:", err)
+		}
+	}
+
+	var msgs [3]<-chan amqp.Delivery
+	for i := 0; i < len(msgs); i++ {
+		msgs[i], err = ch.Consume(
+			queue[i].Name,
+			"",
+			true,
+			false,
+			false,
+			false,
+			nil,
+		)
+		if err != nil {
+			log.Fatalln("Withdraw:Error:", err)
+		}
+	}
+
 	return &Handler{
-		ch:  ch,
-		log: log.New(os.Stdout, "", log.LstdFlags),
+		ch:    ch,
+		log:   log.New(os.Stdout, "", log.LstdFlags),
+		queue: queue,
+		msgs:  msgs,
 	}
 }
 
@@ -33,14 +82,6 @@ func serverResponse(c *gin.Context, code int, message string, result interface{}
 	})
 }
 
-func randomString(l int) string {
-	bytes := make([]byte, l)
-	for i := 0; i < l; i++ {
-		bytes[i] = byte(65 + rand.Intn(25))
-	}
-	return string(bytes)
-}
-
 func (h *Handler) Withdraw(c *gin.Context) {
 	var transaction tr.Transaction
 	if err := c.BindJSON(&transaction); err != nil {
@@ -49,46 +90,17 @@ func (h *Handler) Withdraw(c *gin.Context) {
 		return
 	}
 
-	q, err := h.ch.QueueDeclare(
-		"",    // name
-		false, // durable
-		false, // delete when unused
-		true,  // exclusive
-		false, // noWait
-		nil,   // arguments
-	)
-	if err != nil {
-		h.log.Println("Withdraw:Error:", err)
-		serverResponse(c, http.StatusInternalServerError, "Failed to declare a queue", "", err.Error())
-		return
-	}
-
-	msgs, err := h.ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	if err != nil {
-		h.log.Println("Withdraw:Error:", err.Error())
-		serverResponse(c, http.StatusInternalServerError, "Failed to register a consumer", "", err.Error())
-		return
-	}
-
-	corrId := randomString(32)
+	corrId := shortid.MustGenerate()
 	body, _ := json.Marshal(transaction)
-	err = h.ch.Publish(
-		"",         // exchange
-		"withdraw", // routing key
-		false,      // mandatory
-		false,      // immediate
+	err := h.ch.Publish(
+		"",
+		"withdraw",
+		false,
+		false,
 		amqp.Publishing{
 			ContentType:   "application/json",
 			CorrelationId: corrId,
-			ReplyTo:       q.Name,
+			ReplyTo:       h.queue[0].Name,
 			Body:          body,
 		})
 	if err != nil {
@@ -97,10 +109,10 @@ func (h *Handler) Withdraw(c *gin.Context) {
 		return
 	}
 
-	for d := range msgs {
+	for d := range h.msgs[0] {
+		h.log.Printf("%s: %s", d.CorrelationId, d.Body)
 		if corrId == d.CorrelationId {
 			err = json.Unmarshal(d.Body, &transaction)
-			// err = json.NewDecoder(d.Body).Decode(&transaction)
 			if err != nil {
 				h.log.Println("Withdraw:Error:", err.Error())
 				serverResponse(c, http.StatusInternalServerError, "Failed to convert body to integer", "", err.Error())
@@ -114,9 +126,87 @@ func (h *Handler) Withdraw(c *gin.Context) {
 }
 
 func (h *Handler) Replenishment(c *gin.Context) {
+	var transaction tr.Transaction
+	if err := c.BindJSON(&transaction); err != nil {
+		h.log.Println("Replenishment:Error:", err)
+		serverResponse(c, http.StatusInternalServerError, "", "", err.Error())
+		return
+	}
 
+	corrId := shortid.MustGenerate()
+	body, _ := json.Marshal(transaction)
+	err := h.ch.Publish(
+		"",
+		"replenishment",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType:   "application/json",
+			CorrelationId: corrId,
+			ReplyTo:       h.queue[1].Name,
+			Body:          body,
+		})
+	if err != nil {
+		h.log.Println("Replenishment:Error:", err.Error())
+		serverResponse(c, http.StatusInternalServerError, "Failed to publish a message", "", err.Error())
+		return
+	}
+
+	for d := range h.msgs[1] {
+		h.log.Printf("%s: %s", d.CorrelationId, d.Body)
+		if corrId == d.CorrelationId {
+			err = json.Unmarshal(d.Body, &transaction)
+			if err != nil {
+				h.log.Println("Replenishment:Error:", err.Error())
+				serverResponse(c, http.StatusInternalServerError, "Failed to convert body to integer", "", err.Error())
+				return
+			}
+			break
+		}
+	}
+
+	serverResponse(c, http.StatusOK, "", transaction, "")
 }
 
 func (h *Handler) CardToCard(c *gin.Context) {
+	var transaction tr.Transaction
+	if err := c.BindJSON(&transaction); err != nil {
+		h.log.Println("CardToCard:Error:", err)
+		serverResponse(c, http.StatusInternalServerError, "", "", err.Error())
+		return
+	}
 
+	corrId := shortid.MustGenerate()
+	body, _ := json.Marshal(transaction)
+	err := h.ch.Publish(
+		"",
+		"cardtocard",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType:   "application/json",
+			CorrelationId: corrId,
+			ReplyTo:       h.queue[1].Name,
+			Body:          body,
+		})
+	if err != nil {
+		h.log.Println("CardToCard:Error:", err.Error())
+		serverResponse(c, http.StatusInternalServerError, "Failed to publish a message", "", err.Error())
+		return
+	}
+
+	for d := range h.msgs[1] {
+		h.log.Printf("%s: %s", d.CorrelationId, d.Body)
+		if corrId == d.CorrelationId {
+			err = json.Unmarshal(d.Body, &transaction)
+			if err != nil {
+				h.log.Println("CardToCard:Error:", err.Error())
+				serverResponse(c, http.StatusInternalServerError, "Failed to convert body to integer", "", err.Error())
+				return
+			}
+			break
+		}
+	}
+
+	serverResponse(c, http.StatusOK, "", transaction, "")
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/gin-gonic/gin v1.7.3
 	github.com/streadway/amqp v1.0.0
+	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125
 )
